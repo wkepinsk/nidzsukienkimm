e4d2524455816fbd0910e9242ac8e6470c2310cc
wkepinsk <wkepinsk@gmail.com>
2020-04-26 14:08:15 +0200

    dadanie zamykania popup-a

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from time import  sleep
 
 
 
@@ -14,14 +12,23 @@ class MojPrzypadek(unittest.TestCase):
         self.driver = webdriver.Firefox()
         self.driver.maximize_window()
         self.driver.get('https://www.sukienkimm.pl/sukienki/midi/strona-2/')
+
     def testNiePoprawneHaslo(self):
         driver = self.driver
+        al = driver.find_element_by_id('onesignal-popover-cancel-button')
+        al.click()
         zaloguj_btn = driver.find_element_by_xpath('//span[text()=" zaloguj się"]')
         zaloguj_btn.click()
         print("test wlasciwy")
+        sleep(2)
+
     def tearDown(self) -> None:
         print("sprzatanie po tescie")
         self.driver.quit()
+
+
+
+
 
 if __name__=='__main__':
     unittest.main()
